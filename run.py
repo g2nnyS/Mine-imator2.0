7e8a99dbd7c5bba3f984fe38c9ca09584980fb43
g2nnyS <XianYuPigeon@outlook.com>
2022-10-01 17:14:17 +0800

    这辈子有了

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ if whnd != 0:
 if __name__ == '__main__':
     app = QApplication([])
     app.setApplicationDisplayName('Mine-imator 2.0') #窗口标题
-    app.setWindowIcon(QIcon('1.png'))
+    app.setWindowIcon(QIcon('Data/1.ico')) #窗口图标
     player = QMediaPlayer()
     wgt_video = QVideoWidget()  # 视频显示的widget
     wgt_video.show()
